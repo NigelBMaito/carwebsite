8ec8eedec7ed2689f66ed0bfe81f15c454fcb78c
Nigel <nigelmaito@gmail.com>
2024-11-28 16:49:17 +0000

    Factories complete

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,8 @@ use App\Models\CarImage;
 use App\Models\CarType;
 use App\Models\FuelType;
 use App\Models\Maker;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 
@@ -42,9 +44,9 @@ class HomeController extends Controller
 
       //dd($car->carType);
 
-      $car = Car::find(1);
+    //  $car = Car::find(1);
 
-      dd($car->favouredUsers);
+      //dd($car->favouredUsers);
       // $carType = CarType::where('name', 'Sedan')->first();
 
       // $car->car_type_id = $carType->id;
@@ -59,6 +61,36 @@ class HomeController extends Controller
      // $cars = Car::whereBelongsTo($carType)->get();
 
       //dd($cars);
+
+      // $maker = Maker::factory ()->count(10)-> create();
+      // dd($maker);
+
+      // User::factory()->count(10)->create([
+      //   "name"=> "Nigel",
+      // ]);
+
+      // Maker::factory()
+      // ->count(5)
+      // ->create();
+
+    //  $maker = Maker::factory()
+    //   ->count(5)
+    //   ->hasModels(5)
+    //   ->create();
+
+    //   Maker::factory()
+    // ->count(5)
+    // ->has(Model::factory()->count(5), 'models') // Correct usage
+    // ->create();
+
+    User::factory()
+      ->has(Car::factory()->count(5),'favouriteCars')
+      ->create();
+
+    Maker::factory()->create();
+
+    
+
 
 
          return view("home.index");
