e17a2a114d48a37eeb0e9f3338cf6c16481175ce
Nigel <nigelmaito@gmail.com>
2024-12-10 22:10:35 +0000

    Pagination for search pages

--- resources/views/pagination.blade.php
+++ resources/views/pagination.blade.php
@@ -1,35 +1,59 @@
 <?php
- /** @var $paginator \Illuminate\Pagination\LengthAwarePaginator */
+/** @var $paginator \Illuminate\Pagination\LengthAwarePaginator */
 ?>
-@if($paginator->hasmorePages())
+@if ($paginator->hasPages())
 <nav class="pagination my-large">
-    @if($paginator->onFirstPage())
+    {{-- Previous Page Link --}}
+    @if ($paginator->onFirstPage())
     <span class="pagination-item">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-          stroke="currentColor" style="width: 18px">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+            stroke="currentColor" style="width: 18px">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
         </svg>
     </span>
     @else
-
-    <a href="{{$paginator->previousPageUrl()}}" class="pagination-item">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-        stroke="currentColor" style="width: 18px">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-      </svg>
+    <a href="{{ $paginator->previousPageUrl() }}" class="pagination-item">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" style="width: 18px">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+        </svg>
     </a>
     @endif
 
-    <a href="#" class="pagination-item"> 1 </a>
-    <a href="#" class="pagination-item"> 2 </a>
-    <span class="pagination-item active"> 3 </span>
-    <a href="#" class="pagination-item"> 4 </a>
-    <a href="#" class="pagination-item">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-        stroke="currentColor" style="width: 18px">
-        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-      </svg>
+    {{-- Pagination Elements --}}
+    @foreach ($elements as $element)
+        {{-- "Three Dots" Separator --}}
+        @if (is_string($element))
+        <span class="pagination-item">{{ $element }}</span>
+        @endif
+
+        {{-- Array of Links --}}
+        @if (is_array($element))
+            @foreach ($element as $page => $url)
+                @if ($page == $paginator->currentPage())
+                <span class="pagination-item active">{{ $page }}</span>
+                @else
+                <a href="{{ $url }}" class="pagination-item">{{ $page }}</a>
+                @endif
+            @endforeach
+        @endif
+    @endforeach
+
+    {{-- Next Page Link --}}
+    @if ($paginator->hasMorePages())
+    <a href="{{ $paginator->nextPageUrl() }}" class="pagination-item">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" style="width: 18px">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+        </svg>
     </a>
-   
-  </nav>
-  @endif
+    @else
+    <span class="pagination-item">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" style="width: 18px">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+        </svg>
+    </span>
+    @endif
+</nav>
+@endif
